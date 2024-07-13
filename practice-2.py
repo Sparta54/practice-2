d34a50a7b8ef340101f4a682da7dd4f584de9cae
Jonathan M Clark <114125281+JonathanMClark@users.noreply.github.com>
2024-07-13 16:01:22 -0700

    Update practice-2.py

--- practice-2.py
+++ practice-2.py
@@ -1,1 +1,10 @@
+# Excellent!
+
+# Write an if-else statement that determines if a string (like "apple") is less than 5 characters long. 
+# Use the function len() to determine the length of the string. 
+# Print an output message that tells you the length of the string if the string is less than 5 characters long. 
+# If the string is longer than 5 characters, print out something that says "string is too long" or something like that.
+
+
+
 
