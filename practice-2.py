acce2755b44b77fab2560f20fad0d0501297b739
Jonathan M Clark <114125281+JonathanMClark@users.noreply.github.com>
2024-07-13 16:21:12 -0700

    Update practice-2.py

--- practice-2.py
+++ practice-2.py
@@ -6,6 +6,26 @@
 # If the string is longer than 5 characters, print out something that says "string is too long" or something like that.
 
 
+# Put len() code here
+
 
 if (string is less than 5 characters):
   # Do something
+else:
+  # Do something else
+
+
+
+
+
+############################################################
+# For later... (Ignore for now)
+
+# Use the following for your code
+fruit_basket = ["apple", "orange", "kiwi", "apple", "lemon", "apple", "apple"]
+search_target = "apple"
+apple_count = 0
+
+# Write a Python script that will count the number of apples in the fruit basket.
+# Use a for loop to loop through each element of the fruit basket list, compare the element to the search target, and if the element is an "apple", increase the apple count by 1.
+
