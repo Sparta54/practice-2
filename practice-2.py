2a6a49345382a743886e62acc1e2b23dfc341a9c
Jonathan M Clark <114125281+JonathanMClark@users.noreply.github.com>
2024-07-13 16:55:38 -0700

    Update practice-2.py

--- practice-2.py
+++ practice-2.py
@@ -1,4 +1,4 @@
-# Write an if-else statement that determines if a string (like "apple") is less than 5 characters long. 
+# Write an if-else statement that determines if a string (like "apples") is less than 5 characters long. 
 # Use the function len() to determine the length of the string. 
 # Print an output message that tells you the length of the string if the string is less than 5 characters long. 
 # If the string is longer than 5 characters, print out something that says "string is too long" or something like that.
@@ -10,7 +10,7 @@ input_string = 'apples'
 len(input_string)
 
 if len(input_string) < 5:   # [JONATHAN] Add len() around the input_string. Remove the word 'characters'. Add a colon (:) at the end of this line.
-    print('string is too short')  # [JONATHAN] This should be indented 
+    print('String is too short')  # [JONATHAN] This should be indented 
 else:
     print('String is too long')
 
@@ -19,6 +19,12 @@ else:
 
 
 
-# [JONATHAN] Excellent! Now, write an if statement that says:
-if (string is less than 5 characters):
+# The first line says: if the length of the input_string is less than 5
+# The expression 'len(input_string) < 5' will evaluate to False because "apples" is not less than 5 characters in length.
+
+if (expression that evaluates to True or False):
+	# Do things if expression evaluates to True
+else:
+	# Do things if expression evaluates to False
+
 
