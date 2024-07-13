c6bb758d4a0ef46d52ff47e11ad2e9b734756e61
Jonathan M Clark <114125281+JonathanMClark@users.noreply.github.com>
2024-07-13 16:42:41 -0700

    Update practice-2.py

--- practice-2.py
+++ practice-2.py
@@ -4,56 +4,21 @@
 # If the string is longer than 5 characters, print out something that says "string is too long" or something like that.
 
 
+# [JONATHAN] Delete my notes here when you update it.
+
 input_string = 'apples'
 len(input_string)
-if input_string < 5 characters
-print('string is too short')
+
+if input_string < 5 characters   # [JONATHAN] Add len() around the input_string. Remove the word 'characters'. Add a colon (:) at the end on this line.
+print('string is too short')  # [JONATHAN] This should be indented 
 else:
     print('String is too long')
 
+
+
+
+
+
 # [JONATHAN] Excellent! Now, write an if statement that says:
 if (string is less than 5 characters):
-
-
-
-
-
-
-
-
-
-
-
-
-
-############################################################
-
-# Write an if-else statement that determines if a string (like "apple") is less than 5 characters long. 
-# Use the function len() to determine the length of the string. 
-# Print an output message that tells you the length of the string if the string is less than 5 characters long. 
-# If the string is longer than 5 characters, print out something that says "string is too long" or something like that.
-
-
-# Put len() code here
-
-
-if (string is less than 5 characters):
-  # Do something
-else:
-  # Do something else
-
-
-
-
-
-############################################################
-# For later... (Ignore for now)
-
-# Use the following for your code
-fruit_basket = ["apple", "orange", "kiwi", "apple", "lemon", "apple", "apple"]
-search_target = "apple"
-apple_count = 0
-
-# Write a Python script that will count the number of apples in the fruit basket.
-# Use a for loop to loop through each element of the fruit basket list, compare the element to the search target, and if the element is an "apple", increase the apple count by 1.
 
